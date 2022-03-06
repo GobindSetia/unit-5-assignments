8a7274fea97e7df4cdaaeadfe1a19e991b9c9cae
GobindSetia <gobind.setia@gmail.com>
2022-03-06 16:42:21 +0530

    Single Function to handle changes

--- Sprint-1/3.Use State/src/components/counter.js
+++ Sprint-1/3.Use State/src/components/counter.js
@@ -2,31 +2,21 @@ import { useState } from "react";
 
 export function Counter() {
   const [counter, setCounter] = useState(0);
+
+  const handleIncDec = (value) => {
+    if (value == 1 || value == -1) {
+      setCounter(counter + value);
+    }
+    if (value == 2) {
+      setCounter(counter * 2);
+    }
+  };
   return (
     <div>
       <h3>Counter:{counter}</h3>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          setCounter(counter - 1);
-        }}
-      >
-        Decrement
-      </button>
-      <button
-        onClick={() => {
-          setCounter(counter*2);
-        }}
-      >
-        Double
-      </button>
+      <button onClick={() => handleIncDec(1)}>Increment</button>
+      <button onClick={() => handleIncDec(-1)}>Decrement</button>
+      <button onClick={() => handleIncDec(2)}>Double</button>
     </div>
   );
 }
-
